24255f6d94e32fad60ddaff9d064016b14f9656d
Renan Bezerra <renanbg90@gmail.com>
2020-03-13 15:00:50 -0300

    versão 1.1

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './styles.css';
+
 export default class Main extends Component {
+    state = {
+        api: []
+    };
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+         const response = await api.get("/api");
+            this.setState({ api: response.data})
+         //console.log(response.data[0].codigo);
+    };
+
     render(){
-        return <h1> Hello world</h1>
+        const { api } = this.state;
+    return (
+        <div className="product-list">
+            {api.map(product =>(
+                <article key={product._codigo}>
+                    <strong>{product.nomeCasa}</strong>
+                        <p>{product.local}</p>
+
+                        <a href="">Casa de Evento</a>
+                </article>
+                
+                //<h2 key={product._codigo}>{product.nomeCasa}</h2>
+            ))}
+
+        </div>
+    );
+    
     }
 }
